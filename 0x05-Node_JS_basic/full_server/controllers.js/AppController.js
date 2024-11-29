8c508ed3078a55132317903a5b919da3457803ca
Tinayo001 <keiyatinayo@gmail.com>
2024-11-29 19:42:07 +0300

    full_server

--- 0x05-Node_JS_basic/full_server/controllers.js/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers.js/AppController.js
@@ -1,7 +1,12 @@
+/**
+ * Handles the miscellaneous route handlers.
+ * @author Kelvin Mutea <https://github.com/Zulu-inventor33>
+ */
 class AppController {
-  static getHomepage (request, response) {
-    response.status(200).send('Hello Holberton School');
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
 }
 
+export default AppController;
 module.exports = AppController;
